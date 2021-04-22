f164610361c41bd8f8336f75c4969e53ba2d9bfa
Simon Sanchez V <simonsanvil2012@gmai.com>
2021-04-22 17:16:29 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Easily generate matplotlib.pyplot subplot axes directly from iterables',
+    author='Simon E. Sanchez Viloria',
+    license='MIT',
+)
