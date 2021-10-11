4748dd549aaca0ae42286f436fc3a4dc0ab6ec61
Simon Sanchez V <simonsanvil2012@gmail.com>
2021-10-11 14:02:26 +0200

    patched version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='subplotted',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Easily generate matplotlib.pyplot subplot axes directly from iterables',
     author='Simon E. Sanchez Viloria',
     license='MIT',
